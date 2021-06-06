ee9f2a555ab926ed23f264b86ee7fd8520643c7f
MateoRubinstein <mateorubinstein@icloud.com>
2021-06-06 14:00:28 +0200

    useMedia hooks implemented

---
+++ src/hooks/useMedia.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+
+const useIsDesktop = () => {
+	return useMediaQuery({ minWidth: 992 });
+};
+
+const Desktop = ({ children }) => {
+	const isDesktop = useIsDesktop();
+	return <>{isDesktop && children ? children : null}</>;
+};
+
+const useIsTablet = () => {
+	return useMediaQuery({ minWidth: 768, maxWidth: 991 });
+};
+
+const Tablet = ({ children }) => {
+	const isTablet = useIsTablet();
+	return <>{isTablet && children ? children : null}</>;
+};
+
+const useIsMobile = () => {
+	return useMediaQuery({ maxWidth: 767 });
+};
+
+const Mobile = ({ children }) => {
+	const isMobile = useIsMobile();
+	return <>{isMobile && children ? children : null}</>;
+};
+
+const useIsDefault = () => {
+	return useMediaQuery({ minWidth: 768 });
+};
+
+const Default = ({ children }) => {
+	const isNotMobile = useIsDefault();
+	return <>{isNotMobile && children ? children : null}</>;
+};
+
+export {
+	useIsDesktop,
+	Desktop,
+	useIsTablet,
+	Tablet,
+	useIsMobile,
+	Mobile,
+	useIsDefault,
+	Default,
+};
