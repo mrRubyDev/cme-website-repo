5b5a6a1b6ca11e90b889be9ed1c49bae55f163b9
MateoRubinstein <mateorubinstein@icloud.com>
2021-06-15 20:23:32 +0200

    Footer and Reservar optimized

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Logo from "../logo-no-bck.png";
 import paths from "../navigation/paths";
 import { Link } from "react-router-dom";
+import { useIsDesktop, useIsMobile, useIsTablet } from "../hooks/useMedia";
 
 export default function Footer() {
+	const isDesktop = useIsDesktop();
+	const isMobile = useIsMobile();
+	const isTablet = useIsTablet();
 	return (
 		<div
 			style={{
 				height: "20vh",
-				width: "100%",
+				width: "100vw",
 				borderTop: "1px solid lightgrey",
 				display: "flex",
 				alignItems: "center",
@@ -18,7 +22,7 @@ export default function Footer() {
 			<div
 				style={{
 					display: "flex",
-					justifyContent: "center",
+					justifyContent: "space-between",
 					alignItems: "flex-start",
 					height: "80%",
 					marginTop: "2%",
@@ -28,7 +32,8 @@ export default function Footer() {
 					style={{
 						display: "flex",
 						flexDirection: "column",
-						margin: "0px 50px 0px",
+						margin: !isMobile ? "0px 50px 0px" : null,
+						width: isMobile ? "20%" : null,
 					}}
 				>
 					<h4 style={{ marginBottom: "1rem" }}>Preguntas Frecuentes</h4>
@@ -46,7 +51,8 @@ export default function Footer() {
 					style={{
 						display: "flex",
 						flexDirection: "column",
-						margin: "0px 50px 0px",
+						margin: !isMobile ? "0px 50px 0px" : null,
+						width: isMobile ? "20%" : null,
 					}}
 				>
 					<h4 style={{ marginBottom: "1rem" }}>Social</h4>
@@ -73,7 +79,8 @@ export default function Footer() {
 					style={{
 						display: "flex",
 						flexDirection: "column",
-						margin: "0px 50px 0px",
+						margin: !isMobile ? "0px 50px 0px" : null,
+						width: isMobile ? "30%" : null,
 					}}
 				>
 					<h4 style={{ marginBottom: "1rem" }}>Legal</h4>
@@ -102,9 +109,15 @@ export default function Footer() {
 						Condiciones de Uso
 					</Link>
 				</div>
-				<div style={{ height: "80%", width: "10%", marginLeft: "2rem" }}>
-					<img src={Logo} alt="CME" style={{ height: "100%", width: "100%" }} />
-				</div>
+				{!isMobile && !isTablet && (
+					<div style={{ height: "80%", width: "10%", marginLeft: "2rem" }}>
+						<img
+							src={Logo}
+							alt="CME"
+							style={{ height: "100%", width: "100%" }}
+						/>
+					</div>
+				)}
 			</div>
 		</div>
 	);
--- src/components/screens/Reservar.jsx
+++ src/components/screens/Reservar.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import { buttonBlue, matteBlue } from "../../config/styles";
 import { useForm } from "react-hook-form";
 import Footer from "../Footer";
+import { useIsDesktop, useIsTablet } from "../../hooks/useMedia";
 export default function Reservar() {
+	const isDesktop = useIsDesktop();
+	const isTablet = useIsTablet();
 	const {
 		register,
 		handleSubmit,
@@ -12,112 +15,146 @@ export default function Reservar() {
 	const onSubmit = data => console.log(data);
 	const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
 	return (
-		<div style={{ width: "90%", marginTop: 10, color: matteBlue }}>
-			<form style={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
-				<h2>Pide cita con nosotros</h2>
-				<div style={{ width: "100%", marginTop: 15 }}>
-					<label>Nombre</label>
-					<input
-						type="text"
-						name="name"
-						{...register("name")}
-						style={styles.input}
-					/>
-				</div>
-				<div style={{ width: "100%" }}>
-					<label>Apellidos</label>
-					<input
-						type="text"
-						name="secondName"
-						{...register("secondName")}
-						style={styles.input}
-					/>
-				</div>
-				<div style={{ width: "100%" }}>
-					<label>Cédula</label>
-					<input
-						type="text"
-						name="id"
-						{...register("id")}
-						style={styles.input}
-					/>
-				</div>
-				<div style={{ width: "100%" }}>
-					<label>Correo electrónico</label>
-					<input
-						type="email"
-						name="email"
-						{...register("email")}
-						style={styles.input}
-					/>
-				</div>
-				<div style={{ width: "100%" }}>
-					<label>Teléfono</label>
-					<input
-						type="text"
-						name="phone"
-						required
-						{...register("phone")}
-						style={styles.input}
-					/>
-				</div>
-				<div
-					style={{ width: "100%", display: "flex", flexDirection: "column" }}
+		<div
+			style={{
+				display: "flex",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			<div
+				style={{
+					width: "90%",
+					marginTop: 10,
+					color: matteBlue,
+				}}
+			>
+				<form
+					style={
+						isDesktop
+							? styles.formContainer
+							: isTablet
+							? styles.formContainerTablet
+							: styles.formContainerMobile
+					}
+					onSubmit={handleSubmit(onSubmit)}
 				>
-					<label>Servicio</label>
-					<select
-						name="services"
-						style={styles.input}
-						required
-						{...register("services")}
+					<h2>Pida cita con nosotros</h2>
+					<div style={{ width: "100%", marginTop: 15 }}>
+						<label>Nombre</label>
+						<input
+							type="text"
+							name="name"
+							{...register("name")}
+							style={styles.input}
+						/>
+					</div>
+					<div style={{ width: "100%" }}>
+						<label>Apellidos</label>
+						<input
+							type="text"
+							name="secondName"
+							{...register("secondName")}
+							style={styles.input}
+						/>
+					</div>
+					<div style={{ width: "100%" }}>
+						<label>Cédula</label>
+						<input
+							type="text"
+							name="id"
+							{...register("id")}
+							style={styles.input}
+						/>
+					</div>
+					<div style={{ width: "100%" }}>
+						<label>Correo electrónico</label>
+						<input
+							type="email"
+							name="email"
+							{...register("email")}
+							style={styles.input}
+						/>
+					</div>
+					<div style={{ width: "100%" }}>
+						<label>Teléfono</label>
+						<input
+							type="text"
+							name="phone"
+							required
+							{...register("phone")}
+							style={styles.input}
+						/>
+					</div>
+					<div
+						style={{ width: "100%", display: "flex", flexDirection: "column" }}
 					>
-						<option value="volvo">Volvo</option>
-						<option value="saab">Saab</option>
-						<option value="opel">Opel</option>
-						<option value="audi">Audi</option>
-					</select>
-				</div>
-				<div
-					style={{ width: "100%", display: "flex", flexDirection: "column" }}
-				>
-					<label>Fecha</label>
-					<input
-						type="date"
-						name="date"
-						required
-						style={styles.input}
-						value={date}
-						min={date}
-						{...register("date")}
-						max="2022-12-31"
-						onChange={e => setDate(e.target.value)}
-					/>
-				</div>
-				{/* <input name="date" placeholder="Fecha" required style={styles.input} ref = {register}/> */}
-				<div style={{ width: "100%" }}>
-					<label>Comentarios</label>
-					<textarea
-						type="text-area"
-						name="comments"
-						{...register("comments")}
-						style={styles.inputObser}
-					/>
-				</div>
-				<input type="submit" style={styles.btn} />
-			</form>
+						<label>Servicio</label>
+						<select
+							name="services"
+							style={styles.input}
+							required
+							{...register("services")}
+						>
+							<option value="volvo">Volvo</option>
+							<option value="saab">Saab</option>
+							<option value="opel">Opel</option>
+							<option value="audi">Audi</option>
+						</select>
+					</div>
+					<div
+						style={{ width: "100%", display: "flex", flexDirection: "column" }}
+					>
+						<label>Fecha</label>
+						<input
+							type="date"
+							name="date"
+							required
+							style={styles.input}
+							value={date}
+							min={date}
+							{...register("date")}
+							max="2022-12-31"
+							onChange={e => setDate(e.target.value)}
+						/>
+					</div>
+					{/* <input name="date" placeholder="Fecha" required style={styles.input} ref = {register}/> */}
+					<div style={{ width: "100%" }}>
+						<label>Comentarios</label>
+						<textarea
+							type="text-area"
+							name="comments"
+							{...register("comments")}
+							style={styles.inputObser}
+						/>
+					</div>
+					<input type="submit" style={styles.btn} />
+				</form>
+			</div>
 			<Footer />
 		</div>
 	);
 }
 
+const formContainerBasic = {
+	margin: "3rem auto 0",
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	marginBottom: "2rem",
+};
 const styles = {
 	formContainer: {
+		...formContainerBasic,
 		width: "30%",
-		margin: "3rem auto 0",
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		marginBottom: "2rem",
+	},
+	formContainerMobile: {
+		width: "80%",
+		...formContainerBasic,
+	},
+	formContainerTablet: {
+		width: "60%",
+		...formContainerBasic,
 	},
 
 	input: {
@@ -144,7 +181,8 @@ const styles = {
 		color: "white",
 		border: "none",
 		borderRadius: 5,
-		padding: 5,
+		padding: 10,
+		fontSize: 20,
 		width: "50%",
 	},
 };
